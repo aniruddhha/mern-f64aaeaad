a204e6d166b817abbf60eea9b6dba1a6d0268eaa
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-12-19 20:57:37 +0530

    javascript introduction

---
+++ javascript-training/hi.js
@@ -0,0 +1,14 @@
+// look at the extention *.js
+
+/**
+ * Doc Comment
+ * 
+ */
+
+// console.log('Hey Hi');
+a = 10; c = 10;
+b = 20;
+console.log(a + b);
+// console.log { a + b }
+
+jkasfhslakjdfhsdljk = 90;
