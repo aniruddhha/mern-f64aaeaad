bbc51c18d0a407f0ea5ebf2833a2c2fbf7c16e7f
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-12-23 20:52:53 +0530

    function : anonymus function

---
+++ javascript-training/mrfn.js
@@ -0,0 +1,16 @@
+function nmd(nm) {
+    return '#' + nm.toUpperCase() + '$'
+}
+
+const anFn = function(nm) {
+    return '#' + nm.toUpperCase() + '$'
+}
+
+const cal = 10
+
+console.log(nmd('abc'))
+console.log(anFn('pqr'))
+console.log(anFn)
+console.log(cal)
+
+// ES6
