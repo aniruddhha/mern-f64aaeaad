cc216690a8fbb0c80cbf45d11e4631d6a5277867
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-12-23 20:16:36 +0530

    js : typecasting and builtin functions

---
+++ javascript-training/bltfn.js
@@ -0,0 +1,13 @@
+console.log('Hey Hi')
+
+const a = '10'
+const num1 = Number.parseInt(a)
+const b = '20'
+const num2 =  Number.parseInt(b)
+console.log(a + b)
+console.log(num1 + num2)
+
+const isOkay = true
+console.log(''+isOkay)
+console.log(isOkay.toString())
+console.log((123).toString())
