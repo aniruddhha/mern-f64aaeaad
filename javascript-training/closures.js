d99b7c07f380df5b836db4bc96653b83c4b1c768
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-12-27 20:25:15 +0530

    javascript : closures

---
+++ javascript-training/closures.js
@@ -0,0 +1,32 @@
+function linearData() { // outer function
+    const arr = [-3,-2, -1, 0, 1, 2, 3 ]
+    console.log(arr)
+
+    function plotBarChart() {
+        console.log(arr)
+        console.log(arr)
+    }
+
+    function plotPieChart() {
+        console.log(arr)
+        console.log(arr)
+        console.log(arr)
+    }
+
+    function plotLineChart() {
+        console.log(arr)
+        console.log(arr)
+        console.log(arr)
+        console.log(arr)
+    }
+
+    plotLineChart()
+    plotPieChart()
+    plotBarChart()
+}
+
+linearData()
+
+
+
+
