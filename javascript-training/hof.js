5fa753ab39ee7186a8c438e906278fa8203810bc
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-12-27 20:55:54 +0530

    js : higher order functions

---
+++ javascript-training/hof.js
@@ -0,0 +1,29 @@
+
+function avgSum1() { // function is returning another function
+    let delta = 10
+
+    // return function() {
+    //     return 10 * delta
+    // }
+
+    return () => 10 * delta++
+}
+
+function avgSum2(sm) { // function is passed as parameter to other function
+    sm()
+}
+
+const avg1 = avgSum1()
+avgSum1() // delta = 10
+avgSum1() // delta = 10
+avgSum1() // delta = 10
+avgSum1() // delta = 10
+console.log(avg1())
+
+const calcDelta = () => 0.5 // function is assigned to varible
+avgSum2(calcDelta)
+avgSum2(function() {
+    return 0.5
+})
+avgSum2( () => 0.5 )
+
