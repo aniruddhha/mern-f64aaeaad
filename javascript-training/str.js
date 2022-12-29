a2f9331ec98aaea04bf6972a9905ea40dcaf6cb4
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-12-29 20:51:00 +0530

    js : stubstr and slice

--- javascript-training/str.js
+++ javascript-training/str.js
@@ -8,3 +8,24 @@ console.log(carNm.repeat(3))
 console.log(nm.charAt(3))
 console.log(carNm.split(''))
 console.log(nm.substring(1, 2))
+
+// few more operations
+
+console.log('🔥 29 Dec 2022 🔥')
+// console.log('android'.substring(3,1))
+// console.log('android'.slice(3,1))
+
+// console.log('android'.substring(-100, -2)) // st = 0, 2
+// console.log('android'.substring(0, 0)) 
+console.log('android'.slice(3,0))
+// exstr = length - abs(stop)
+// 3, -3 = r => 7 - 3 = 4 (3, 4)
+// 3, -2 = ro => 7 - 2 = 5 (3, 5)
+// 3, -1 = roi => 7 - 1 = 6 (3, 6)
+// 3, 0 = ''
+
+// content manipulation
+const mobs = 'abc,pqr,xyz,lmn,rty'
+const exOp = ['abc', 'pqr', 'xyz', 'lmn', 'rty']
+
+
