a545a8555bf48f6514486de23c4c0c02b0e64849
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-12-29 20:20:02 +0530

    js : string interpolation

---
+++ javascript-training/strinterploation.js
@@ -0,0 +1,9 @@
+// const fnNm = 'abc'
+const lsNm = 'pqr'
+const flNm1 = 'My name is '+fnNm + ' ' + lsNm
+console.log(flNm1)
+
+// es6 - string interploation
+const flNm2 = `My Name is ${fnNm} ${lsNm}`
+const res1 = `Addition ${10 + 10}`
+const res2 = `Uppercase ${fnNm.toUpperCase()}`
