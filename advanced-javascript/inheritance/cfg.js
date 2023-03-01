d0aa2f31b7991e9ab5e22fb681009fe73720622b
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-03-01 20:55:16 +0530

    class : static and non static

--- advanced-javascript/inheritance/cfg.js
+++ advanced-javascript/inheritance/cfg.js
@@ -7,12 +7,16 @@ class Config {
         this.#pass = pass // it is private to class
     }
 
-    printCreds() {
+    get usNm() {
+        return this.#usNm
+    }
+
+    printCreds() { // this method accessed by object
         console.log(`UserName ${this.#usNm}, Password ${this.#pass}`)
     }
 
-    get usNm() {
-        return this.#usNm
+    static notForInstance() { // to call this method, object is not required
+        console.log(`I dont need instance`)
     }
 }
 
@@ -20,7 +24,9 @@ class DbConfig extends Config { }
 
 class NwConfig extends Config { }
 
-const cfg = new Config('abc', 'pqr')
-console.log(`Outside`)
-console.log(`UserName ${cfg.usNm}, Password ${cfg.pass}`)
-cfg.printCreds()
+Config.notForInstance()
+
+// const cfg = new Config('abc', 'pqr')
+// console.log(`Outside`)
+// console.log(`UserName ${cfg.usNm}, Password ${cfg.pass}`)
+// cfg.printCreds()
