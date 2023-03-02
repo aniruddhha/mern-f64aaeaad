49a9d17c8ee07c17cf4e5e4640e802c02ac4fa78
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-03-02 20:25:21 +0530

    hoisting : explanation

---
+++ advanced-javascript/hoisting/hoisting.js
@@ -0,0 +1,18 @@
+ajhsfgjsadkhfg = "abc"; // initilization
+
+const url = `${ajhsfgjsadkhfg} is value` // using 
+console.log(url)
+
+var ajhsfgjsadkhfg; // declartaion
+
+
+foo() // call
+
+
+function foo() { // define
+    console.log('Foo Called')
+}
+
+var a;
+a = 67
+console.log(a)
